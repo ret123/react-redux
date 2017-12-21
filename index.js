b685c6955e323290c16fbe3276ce1d4239f07108
unknown <rathish83@gmail.com>
2017-12-21 13:19:16 +0530

    Google Oauth2 implemented

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
+require('./services/passport');
+
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send({ hi: "bye " });
-});
-
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
