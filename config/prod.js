7aed2465e1b319f947cddafe46f360049f1aa32b
unknown <rathish83@gmail.com>
2018-01-17 21:16:17 +0530

    set MONGODB_URI

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: "mongodb://ret123:fedora11@ds247317.mlab.com:47317/ret-emaily-prod",
+  mongoURI: process.env.MONGODB_URI,
   cookieKey: process.env.COOKIE_KEY
 };
