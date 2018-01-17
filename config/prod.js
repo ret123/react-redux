b0d14ace316e213d22f57c4893fcd6389a5345af
unknown <rathish83@gmail.com>
2018-01-17 21:12:29 +0530

    prod mongo key change

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: "mongodb://ret123:fedora11@ds247317.mlab.com:47317/ret-emaily-prod",
   cookieKey: process.env.COOKIE_KEY
 };
