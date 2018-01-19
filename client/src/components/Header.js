9dbbb04081f1f52082f1ae3ad91a96c0c97279f9
ret123 <rathish83@gmail.com>
2018-01-19 18:32:30 +0530

    Stripe payment gateway added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,42 @@
 import React, {Component}  from 'react';
+import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
+import Stripe from './Stripe'
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login with Google</a></li>;
+      default:
+        return [
+        <li key="1"><Stripe /></li>,
+        <li key="2"><a href="/api/logout">Log Out</a></li>
+      ];
+    }
+  }
   render() {
     return (
-      <div>
-       Header
-       </div>
+      <nav>
+        <div className="nav-wrapepr">
+          <Link
+           to = {this.props.auth ? '/surveys' : '/'}
+           className="brand-logo left"
+           >
+            Emaily
+          </Link>
+          <ul className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+       </nav>
     );
   }
 }
-export default Header;
+function mapStateToProps({auth}) {
+  return {auth};
+}
+
+export default connect(mapStateToProps)(Header);
