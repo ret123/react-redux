1c996adb527fa2700abd426622ff07f66bfc6d15
ret123 <rathish83@gmail.com>
2018-02-02 17:13:49 +0530

    User can see survey List

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,49 +1,81 @@
-const _ = require('lodash');
-const Path = require('path-parser');
-const {URL} = require('url');
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-const requireCredits = require('../middlewares/requireCredits');
-const Mailer = require('../services/Mailer');
-const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const _ = require("lodash");
+const Path = require("path-parser");
+const { URL } = require("url");
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
-const Survey = mongoose.model('surveys');
+const Survey = mongoose.model("surveys");
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req,res) => {
-    res.send('Thanks for voting!');
-  });
-
-  app.post('/api/surveys/webhooks', (req,res) => {
-    const events = _.map(req.body, event => {
-      const pathname = new URL(event.url).pathname;
-      const p = new Path('/api/surveys/:surveyId/:choice');
-      console.log(p.test(pathname));
-    });
-  });
-
-  app.post('/api/surveys', requireLogin,requireCredits, (req,res) => {
-      const { title, subject, body, recipients } = req.body;
-      const survey = new Survey ({
-        title,
-        subject,
-        body,
-        recipients: recipients.split(',').map(email => ({ email })),
-        _user: req.user.id,
-        dateSent: Date.now()
+  app.get("/api/surveys", requireLogin, (req, res) => {
+    Survey.find({ _user: req.user.id })
+      .select({ recipients: false })
+      .then(surveys => {
+        res.send(surveys);
       });
-      const mailer = new Mailer(survey, surveyTemplate(survey));
-      try {
-           mailer.send();
-           survey.save()
-           .then(() =>  req.user.credits -= 1)
-           .then(() => req.user.save())
-           .then ((user) => {
-             console.log(user);
-             res.send(user);
+  });
+
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
+    res.send("Thanks for voting!");
+  });
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const p = new Path("/api/surveys/:surveyId/:choice");
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy("email", "surveyId")
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
+      .value();
+
+    res.send({});
+  });
+
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => ({ email })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    try {
+      mailer.send();
+      survey
+        .save()
+        .then(() => (req.user.credits -= 1))
+        .then(() => req.user.save())
+        .then(user => {
+          console.log(user);
+          res.send(user);
         });
     } catch (err) {
-        console.log(err);
-        res.status(422).send(err);
-      }
-    });
+      console.log(err);
+      res.status(422).send(err);
+    }
+  });
 };
